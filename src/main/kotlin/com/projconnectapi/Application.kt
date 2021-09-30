01db08da48986ed2a38c5447eb3524f613096d5f
João Seródio <joaoserodio@live.com>
2021-09-30 00:20:42 -0300

    refactor Serialization plugin installation

--- src/main/kotlin/com/projconnectapi/Application.kt
+++ src/main/kotlin/com/projconnectapi/Application.kt
@@ -1,24 +1,15 @@
 package com.projconnectapi
 
-import com.fasterxml.jackson.databind.SerializationFeature
 import com.projconnectapi.plugins.configureOAuth
 import com.projconnectapi.plugins.configureRouting
+import com.projconnectapi.plugins.configureSerialization
 import com.projconnectapi.plugins.configureSessions
-import io.ktor.application.install
-import io.ktor.features.ContentNegotiation
-import io.ktor.jackson.jackson
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
-import org.litote.kmongo.id.jackson.IdJacksonModule
 
 fun main() {
     embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
-        install(ContentNegotiation) {
-            jackson {
-                enable(SerializationFeature.INDENT_OUTPUT)
-                registerModule(IdJacksonModule())
-            }
-        }
+        configureSerialization()
         configureOAuth()
         configureRouting()
         configureSessions()
