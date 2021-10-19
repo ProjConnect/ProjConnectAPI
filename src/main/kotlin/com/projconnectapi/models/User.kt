7cf02acbee9887504552c9c6a43b348f3b2f6c34
João Seródio <joaoserodio@live.com>
2021-10-18 23:33:45 -0300

    update User model

--- src/main/kotlin/com/projconnectapi/models/User.kt
+++ src/main/kotlin/com/projconnectapi/models/User.kt
@@ -1,17 +1,22 @@
 package com.projconnectapi.models
 
+import org.bson.codecs.pojo.annotations.BsonId
+import org.litote.kmongo.Id
+import org.litote.kmongo.newId
+
 data class User(
+    @BsonId val _id: Id<User> = newId(),
     val username: String,
     val email: String,
     val isModerator: Boolean,
-    val firstName: String,
-    val lastName: String,
-    val contact: String,
-    val aboutMe: String,
-    var score: Float,
-    var history: List<String>,
-    val technologies: List<String>,
-    val otherSkills: List<String>,
-    val github: String,
-    val gitlab: String
+    val firstName: String?,
+    val lastName: String?,
+    val aboutMe: String?,
+    val score: Float?,
+    val history: List<Id<Post>>?,
+    val languages: List<String>?,
+    val technologies: List<String>?,
+    val otherSkills: List<String>?,
+    val github: String?,
+    val gitlab: String?
 )
