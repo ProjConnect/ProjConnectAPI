798cee54b21e3454bb7c648ce99dc1b10b66a53d
João Seródio <joaoserodio@live.com>
2021-10-18 23:58:11 -0300

    User routes changes

--- src/main/kotlin/com/projconnectapi/routes/User.kt
+++ src/main/kotlin/com/projconnectapi/routes/User.kt
@@ -1,14 +1,18 @@
 package com.projconnectapi.routes
 
-import com.projconnectapi.clients.database
 import com.projconnectapi.clients.tokenVerifier
+import com.projconnectapi.clients.utils.createUser
+import com.projconnectapi.clients.utils.getUser
+import com.projconnectapi.clients.utils.updateUser
 import com.projconnectapi.models.User
+import com.projconnectapi.models.extensions.toPublicUser
+import com.projconnectapi.schemas.PublicUser
 import com.projconnectapi.schemas.UserSession
+import com.projconnectapi.schemas.extensions.toUser
 import io.ktor.application.call
 import io.ktor.http.HttpStatusCode
 import io.ktor.request.receive
 import io.ktor.response.respond
-import io.ktor.response.respondRedirect
 import io.ktor.response.respondText
 import io.ktor.routing.Route
 import io.ktor.routing.get
@@ -16,31 +20,19 @@ import io.ktor.routing.post
 import io.ktor.sessions.get
 import io.ktor.sessions.sessions
 import org.litote.kmongo.eq
-import org.litote.kmongo.findOne
-import org.litote.kmongo.getCollection
 
-fun isUserSafe(user: User, googleAcc: String, newUser: Boolean): Boolean {
-    return if (newUser) {
-        user.username != "" &&
-            database.getCollection<User>()
-            .findOne(User::username eq user.username) == null &&
-            user.email == googleAcc &&
-            !user.isModerator
-    } else {
-        val userInDB: User? = database.getCollection<User>()
-            .findOne(User::username eq user.username)
-        userInDB != null &&
-            user.username == userInDB.username &&
-            user.email == userInDB.email &&
-            (!user.isModerator || user.isModerator && userInDB.isModerator)
-    }
+fun isUserSafe(user: PublicUser, googleAcc: String): Boolean {
+    return user.username != "" && user.email == googleAcc
 }
 
-fun ifSafeThenInsert(user: User, googleAcc: String): Boolean {
-    return if (isUserSafe(user, googleAcc, true)) {
-        user.score = 0.0f
-        user.history = listOf()
-        database.getCollection<User>().insertOne(user)
+fun ifSafeThenInsert(user: PublicUser, googleAcc: String): Boolean {
+    return if (isUserSafe(user, googleAcc)) {
+        val currentUser = getUser(User::email eq user.email)
+        if (currentUser != null) {
+            updateUser(user.toUser(currentUser))
+        } else {
+            createUser(user.toUser(null))
+        }
         true
     } else {
         false
@@ -54,48 +46,47 @@ fun Route.userRoute() {
 
     get("/my_profile") {
         val userSession: UserSession? = call.sessions.get<UserSession>()
-        if (userSession != null) {
-            val email = tokenVerifier.verify(userSession.idToken).payload["email"]
-            val userProfile: User? = database.getCollection<User>().findOne(User::email eq email)
+        val auth = tokenVerifier.verify(userSession?.idToken)?.payload
+        if (auth != null) {
+            val email = auth["email"].toString()
+            val userProfile = getUser(User::email eq email)
             if (userProfile != null) {
-                call.respond(userProfile)
+                call.respond(userProfile.toPublicUser())
             } else {
                 call.respond(
-                    User(
-                        "",
-                        email as String,
-                        false,
-                        "",
+                    PublicUser(
                         "",
                         email,
-                        "",
-                        0.0f,
-                        listOf<String>(),
-                        listOf<String>(),
-                        listOf<String>(),
-                        "",
-                        ""
+                        null,
+                        null,
+                        null,
+                        null,
+                        null,
+                        null,
+                        null,
+                        null,
                     )
                 )
             }
         } else {
-            call.respondRedirect("/login")
+            call.response.status(HttpStatusCode.Unauthorized)
         }
     }
 
-    post("/sign-up") {
-        var user = call.receive<User>()
+    post("/profile/update") {
+        val user = call.receive<PublicUser>()
         val userSession: UserSession? = call.sessions.get<UserSession>()
-        if (userSession != null) {
-            val email = tokenVerifier.verify(userSession.idToken).payload["email"]
-            val inserted = ifSafeThenInsert(user, email as String)
+        val auth = tokenVerifier.verify(userSession?.idToken)?.payload
+        if (auth != null) {
+            val email = auth["email"].toString()
+            val inserted = ifSafeThenInsert(user, email)
             if (inserted) {
                 call.response.status(HttpStatusCode.Created)
             } else {
                 call.response.status(HttpStatusCode.BadRequest)
             }
         } else {
-            call.respondRedirect("/login")
+            call.response.status(HttpStatusCode.Unauthorized)
         }
     }
 }
