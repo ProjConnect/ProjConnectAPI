70766cd11aa0ff5c3866f36000afe827d626f1d6
João Seródio <joaoserodio@live.com>
2021-12-02 14:32:18 -0300

    Infractor DB model and operations

---
+++ src/main/kotlin/com/projconnectapi/models/Infractor.kt
@@ -0,0 +1,12 @@
+package com.projconnectapi.models
+
+import org.bson.codecs.pojo.annotations.BsonId
+import org.litote.kmongo.Id
+import org.litote.kmongo.newId
+
+data class Infractor(
+    @BsonId val _id: Id<Infractor> = newId(),
+    val user: String,
+    var banStatus: Boolean,
+    var infractions: MutableList<String>,
+)
