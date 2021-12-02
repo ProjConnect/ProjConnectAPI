64bfeef46004e0bc00dde0dc097d66a8f906fee3
João Seródio <joaoserodio@live.com>
2021-12-02 04:12:33 -0300

    add new fields to post

--- src/main/kotlin/com/projconnectapi/schemas/NewPost.kt
+++ src/main/kotlin/com/projconnectapi/schemas/NewPost.kt
@@ -4,9 +4,11 @@ data class NewPost(
     val subject: String,
     val ownerId: String,
     val devId: MutableList<String>,
+    val deadline: String?,
     val body: String,
     val supporters: List<String>,
     val isArchived: Boolean,
     val tags: List<String>,
     val course: String,
+    val image: Int,
 )
